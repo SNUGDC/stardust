64b865ac588fdddbac0089de8dcad630f21ceb18
송용재 <dydwo1127@gmail.com>
2016-01-16 21:47:09 +0900

    다른 팔레트 자동으로 없어지게 설정

--- Metro/Assets/_Scripts/PaletteCaller.cs
+++ Metro/Assets/_Scripts/PaletteCaller.cs
@@ -4,55 +4,78 @@ using System.Collections;
 public class PaletteCaller : MonoBehaviour {
 
 
-	public string tag = "";
+	public string tag;
 
 	GameObject[] Palettes;
 	Vector3[] tempPos;
 	Vector3[] temp;
+	bool back = false;
+	int routine = 0;
 
 	public bool active = false;
-	public float smoothTime = 0.1f;
+	public float smoothTime = 0.18f;
 
 	private Vector3[] velocity;
 
 	void Start () {
 	
-		Palettes = GameObject.FindGameObjectsWithTag (tag);//Making array which collects palette's' belong to some tage
+		Palettes = GameObject.FindGameObjectsWithTag (tag);
 
 		tempPos = new Vector3[Palettes.GetLength(0)];
 		temp = new Vector3[Palettes.GetLength (0)];
 		velocity = new Vector3[Palettes.GetLength (0)];
 		for (int i = 0; i < Palettes.GetLength (0); i++) 
 		{
-			tempPos[i] = Palettes[i].transform.position;//Palettes is array which size is as same as the number of GameObject 'tag'
-		}	//and tempPos[i] remember switched position of Palettes[i]
-
+			tempPos[i] = Palettes[i].transform.position;
+			Palettes [i].SetActive (false);
+		}
 	}
 
-	void Update () {
-
-		foreach (GameObject palette in Palettes) 
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
 		{
-			palette.SetActive (active);
+			if (active == true) 
+			{
+				routine = 1;
+			}
+			else 
+			{
+				routine = 2;
+			}
+			active = false;
 		}
-
-		for (int i = 0; i < Palettes.GetLength(0); i ++) 
+		if (routine == 1) 
 		{
+			PaletteCall ();
+		}
+		if (routine == 2) 
+		{
+			for (int i = 0; i < Palettes.GetLength (0); i++) 
+			{
+				Palettes [i].SetActive (false);
+			}
+		}
+	}
+
+	void PaletteCall ()
+	{
+		for (int i = 0; i<Palettes.GetLength(0); i++) 
+		{
+			Palettes [i].SetActive (true);
 			temp[i] = Vector3.SmoothDamp(Palettes[i].transform.position, tempPos[i], ref velocity[i], smoothTime);
-			//move the palette by frame smoothly(SmoothDamp) and substitude its position value to temp[i]
-			Palettes[i].transform.position = temp[i];//remember position of moved palette per frame 
+			Palettes[i].transform.position = temp[i];
 		}
 	}
 
 	void OnMouseDown()
 	{
-		active = !active;
+		active = true;
 		for (int i = 0; i < Palettes.GetLength(0); i++) 
 		{
 			Palettes[i].transform.position = transform.position;
 		}
 	}
-
 }
 
 
