b9e9e4f0ea1cd13c8ba83105f8b5bff1931105e0
Lahima <Lahima@Lahimaui-MacBook-Pro.local>
2016-02-14 21:57:14 +0900

    동굴. 추가 별 획득 동굴 빼고 완료. 지훈씨 동방 오실 수 있는 날에 설명 하고 애니메이션 넣겠음

---
+++ Stardust/Assets/_Scripts/_Public/touchedPaletteCaller.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaletteCaller : MonoBehaviour {
+
+
+	public string PaletteTag;
+
+	GameObject[] Palettes;
+	Vector3[] tempPos;
+	Vector3[] temp;
+	int routine = 0;
+	public bool active = false;
+	public float smoothTime = 0.18f;
+
+	private Vector3[] velocity;
+
+	public enum eState
+	{
+		None,
+
+		Contact
+	}
+
+	eState mushroomState;
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player") 
+		{
+			mushroomState = eState.Contact;
+		} 
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		mushroomState = eState.None;
+	}
+	void Update()
+	{
+		Touch ();
+		if (routine == 1) 
+		{
+			PaletteCall ();
+		}
+
+		if (routine == 2) 
+		{
+			for (int i = 0; i < Palettes.GetLength (0); i++) 
+			{
+				Palettes [i].SetActive (false);
+			}
+		}
+	}
+	void OnMouseDown()//change start position of Palette
+	{
+		active = true;
+		for (int i = 0; i < Palettes.GetLength(0); i++) 
+		{
+			Palettes[i].transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+	}
+
+	void Touch()
+	{
+		if (mushroomState == eState.Contact && Input.GetMouseButtonDown(0)) 
+		{
+			if (active == true) 
+			{
+				routine = 1;
+			}
+			else 
+			{
+				routine = 2;
+			}
+			active = false;
+		}
+	}
+
+	void Start () {
+
+		Palettes = GameObject.FindGameObjectsWithTag (PaletteTag);
+
+		tempPos = new Vector3[Palettes.GetLength(0)];
+		temp = new Vector3[Palettes.GetLength (0)];
+		velocity = new Vector3[Palettes.GetLength (0)];
+		for (int i = 0; i < Palettes.GetLength (0); i++) 
+		{
+			tempPos[i] = Palettes[i].transform.position;
+			Palettes [i].SetActive (false);
+		}
+	}
+
+	void PaletteCall ()
+	{
+		for (int i = 0; i < Palettes.GetLength (0); i++) {
+			Palettes [i].SetActive (true);
+			temp [i] = Vector3.SmoothDamp (Palettes [i].transform.position, tempPos [i], ref velocity [i], smoothTime);
+			Palettes [i].transform.position = temp [i];
+		}
+	}
+}
