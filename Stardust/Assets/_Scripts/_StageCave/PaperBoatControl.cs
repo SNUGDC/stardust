7ad7f4f2d262806e521153f4c76eb4f1510e396c
Lahima <Lahima@Lahimaui-MacBook-Pro.local>
2016-02-19 22:39:29 +0900

    Worked with Yongjae.

---
+++ Stardust/Assets/_Scripts/_StageCave/PaperBoatControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaperBoatControl : MonoBehaviour {
+
+	private bool active;
+	public GameObject Pond;
+
+	// Update is called once per frame
+	void LateUpdate () {
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (active == true)
+			{
+				this.gameObject.SetActive (true);
+
+			}
+			else
+			{
+				GetComponentInParent<touchedTargetCaller> ().enabled = true;
+				this.gameObject.SetActive (false);
+			}
+			active = false;
+		}
+	}
+
+	void OnMouseDown()
+	{
+		active = true;
+		Pond.GetComponent<touchedTargetCaller> ().enabled = false;
+
+	}
+
+}
