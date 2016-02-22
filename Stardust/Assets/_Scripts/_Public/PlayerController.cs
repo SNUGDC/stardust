b4909018add4d823a190c4046df71e1d4ebe7ebc
Song <dydwo1127@gmail.com>
2016-02-23 06:34:30 +0900

    놀이동산 퍼즐 완료

--- Stardust/Assets/_Scripts/_Public/PlayerController.cs
+++ Stardust/Assets/_Scripts/_Public/PlayerController.cs
@@ -25,7 +25,6 @@ public class PlayerController : MonoBehaviour {
 	void MovePlayer()
 	{
 		float touchPositionX = Input.mousePosition.x;
-		Debug.Log (touchPositionX);
 
 		if (Input.mousePosition.x >= (2 * Screen.width / 3) && facingRight)
 		{
