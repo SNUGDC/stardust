f7ab587fe34eb1ff2765d123712390cc88991d4e
Lahima <Lahima@Lahimaui-MacBook-Pro.local>
2016-02-23 07:42:50 +0900

    asdfasf

---
+++ Stardust/Assets/_Scripts/_StageCave/CaveClearStar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveClearStar : MonoBehaviour {
+
+	public GameObject ClearStar;
+	public GameObject ExitTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ExitTarget.activeInHierarchy == true) 
+		{
+			ClearStar.SetActive (true);
+		}
+	}
+}
