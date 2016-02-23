f08aedd56e6628edb3301dea469e9d1ddf762157
Lahima <Lahima@Lahimaui-MacBook-Pro.local>
2016-02-23 09:53:25 +0900

    All stage Clear

--- Stardust/Assets/_Scripts/_StageSelect/StarCollector.cs
+++ Stardust/Assets/_Scripts/_StageSelect/StarCollector.cs
@@ -10,6 +10,11 @@ public class StarCollector : MonoBehaviour {
 	public static int MetroStar;
 	public static int AliceStar;
 	public static int AmusementStar;
+	public GameObject[] AmusementArray;
+	public GameObject[] ForestArray;
+	public GameObject[] CaveArray;
+	public GameObject[] AliceArray;
+
 	private static StarCollector star_instance;
 	private int starCount;
 
@@ -34,7 +39,32 @@ public class StarCollector : MonoBehaviour {
 
 	void Update()
 	{
-		
+		for (int i = 0; i < 6; i++) {
+			if (AmusementStar != i) {
+				AmusementArray [i].SetActive (false);
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			if (CaveStar != i) {
+				CaveArray [i].SetActive (false);
+			}
+		}
+		for (int i = 0; i < 4; i++) 
+		{
+			if (AliceStar != i) {
+				AliceArray [i].SetActive (false);
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			if (ForestStar != i) 
+			{
+				ForestArray [i].SetActive (false);
+			}
+		}
+		AmusementArray [AmusementStar].SetActive (true);
+		CaveArray [CaveStar].SetActive (true);
+		AliceArray [AliceStar].SetActive (true);
+		ForestArray [ForestStar].SetActive (true);
 	}
 
 
