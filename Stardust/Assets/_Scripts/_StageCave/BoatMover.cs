cadeff2e4aa5ceec247c3e1ca9d82d3c7261fca9
JamesKim2998 <eodnjs2998@gmail.com>
2016-02-23 09:01:49 +0900

    동굴버그수정

--- Stardust/Assets/_Scripts/_StageCave/BoatMover.cs
+++ Stardust/Assets/_Scripts/_StageCave/BoatMover.cs
@@ -7,12 +7,13 @@ public class BoatMover : MonoBehaviour
     public Collider2D LeftCollider;
     public Collider2D RightCollider;
     public GameObject BoatFront;
+    public GameObject Torch;
 
     private bool OnBoard = false;
     private bool goright = false;
     private bool goleft = false;
 
-    void Update()
+    void FixedUpdate()
     {
         if (LeftCollider.IsTouching(Player.GetComponent<Collider2D>()))
         {
@@ -23,6 +24,7 @@ public class BoatMover : MonoBehaviour
         if (goright)
         {
             transform.Translate(2*Time.deltaTime,0,0);
+            Torch.transform.position = new Vector3(transform.position.x + 0.8f, transform.position.y+0.5f, 0);
         }
         if (RightCollider.IsTouching(Player.GetComponent<Collider2D>()))
         {
@@ -33,6 +35,7 @@ public class BoatMover : MonoBehaviour
         if (goleft)
         {
             transform.Translate(-2 * Time.deltaTime, 0, 0);
+            Torch.transform.position = new Vector3(transform.position.x - 0.8f, transform.position.y + 0.5f, 0);
         }
 
         if (OnBoard)
@@ -46,13 +49,17 @@ public class BoatMover : MonoBehaviour
         BoatFront.GetComponent<Renderer>().sortingOrder = 3;
         Player.GetComponent<PlayerController>().enabled = false;
         Player.GetComponent<Animator>().enabled = false;
+        Torch.GetComponent<TorchControl>().enabled = false;
+        Player.GetComponent<Rigidbody2D>().isKinematic = true;
         yield return new WaitForSeconds(3);
+        Player.GetComponent<Rigidbody2D>().isKinematic = false;
+        Torch.GetComponent<TorchControl>().enabled = true;
         Player.GetComponent<Animator>().enabled = true;
         Player.GetComponent<PlayerController>().enabled = true;
         OnBoard = false;
         goright = false;
         BoatFront.GetComponent<Renderer>().sortingOrder = 0;
-        Player.transform.position = new Vector3(transform.position.x+5,Player.transform.position.y+2,Player.transform.position.z);
+        Player.transform.position = new Vector3(transform.position.x+5,Player.transform.position.y+1,Player.transform.position.z);
     }
 
     IEnumerator goLeft()
@@ -60,12 +67,16 @@ public class BoatMover : MonoBehaviour
         BoatFront.GetComponent<Renderer>().sortingOrder = 3;
         Player.GetComponent<PlayerController>().enabled = false;
         Player.GetComponent<Animator>().enabled = false;
+        Torch.GetComponent<TorchControl>().enabled = false;
+        Player.GetComponent<Rigidbody2D>().isKinematic = true;
         yield return new WaitForSeconds(3);
+        Player.GetComponent<Rigidbody2D>().isKinematic = false;
+        Torch.GetComponent<TorchControl>().enabled = true;
         Player.GetComponent<Animator>().enabled = true;
         Player.GetComponent<PlayerController>().enabled = true;
         OnBoard = false;
         goleft = false;
         BoatFront.GetComponent<Renderer>().sortingOrder = 0;
-        Player.transform.position = new Vector3(transform.position.x - 5, Player.transform.position.y + 2, Player.transform.position.z);
+        Player.transform.position = new Vector3(transform.position.x - 5, Player.transform.position.y + 1, Player.transform.position.z);
     }
 }
